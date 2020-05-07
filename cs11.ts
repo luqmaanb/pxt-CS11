51ce046f12d369affcda014e4a0deffa80031b00
luqmaanb <lb@xinabox.cc>
2020-05-07 13:32:19 +0200

    .

---
+++ cs11.ts
@@ -0,0 +1,9 @@
+//% color=#444444 icon="\uf0eb"
+//% groups=[Colour,Light, Gesture, Proximity, Optional]
+namespace CS11 {
+
+    //%block="CS11 test block"
+    export function cs11block(): number {
+        return 2;
+    }
+}
